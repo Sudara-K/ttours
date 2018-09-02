b70bf9fb2c556df9925fa59e156c8e1ec03753e0
sudhara <mokatha@gmail.com>
2018-09-02 14:12:29 +1200

    init

---
+++ manage.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import os
+import sys
+import django
+
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'suzzapps.settings')
+    django.setup()
+
+    # Override default port for `runserver` command
+    from django.core.management.commands.runserver import Command as runserver
+    runserver.default_addr='192.168.3.143'
+    runserver.default_port = "8000"
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
